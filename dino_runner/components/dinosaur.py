221a033d14a79652ca3f71c8f901ab864d59a56b
Francy101001 <126831168+Francy101001@users.noreply.github.com>
2023-03-03 19:13:04 -0500

    Second-Class

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -1,26 +1,55 @@
 import pygame
 from pygame.sprite import Sprite
-from dino_runner.utils.constants import RUNNING
+from dino_runner.utils.constants import RUNNING, JUMPING, DUCKING
 
 class Dinosaur(Sprite):
+    X_POS = 80
+    Y_POS = 310
+    JUMP_SPEED = 8.5
     
     def __init__(self):
+        super().__init__()
         self.image = RUNNING[0]
-        self.dino_rect = self.image.get_rect()
-        self.dino_rect.x = 80
-        self.dino_rect.y = 310
-        self.frame_index = 0
-        self.counter = 0
+        self.rect = self.image.get_rect()
+        self.rect.x = self.X_POS
+        self.rect.y = self.Y_POS
+        self.step_index = 0
+        self.dino_run = True
+        self.dino_jump = False
+        self.dino_duck = False
+        self.jump_speed = self.JUMP_SPEED
 
-    def update(self):
-        # Incrementar el contador de frames
-        self.counter += 1
+    def update(self, user_input):
+        if self.dino_run:
+            self.run()
+        elif self.dino_jump:
+            self.jump()
+        
+        if user_input[pygame.K_SPACE] and not self.dino_jump:
+            self.dino_jump = True
+            self.dino_run = False
+        elif not self.dino_jump:
+            self.dino_jump = False
+            self.dino_run = True
 
-        # Cambiar la imagen del dinosaurio cada 10 fotogramas
-        if self.counter % 10 == 0:
-            self.frame_index += 1
-            self.frame_index %= len(RUNNING)
-            self.image = RUNNING[self.frame_index]
+        if self.step_index >= 10:
+            self.step_index = 0
+
+    def run(self):
+        self.image = RUNNING[0] if self.step_index < 5 else RUNNING[1]
+        self.rect.x = self.X_POS
+        self.rect.y = self.Y_POS
+        self.step_index += 1
+    
+    def jump(self):
+        self.image = JUMPING
+        self.rect.y -= int(self.jump_speed * 4)
+        self.jump_speed -= 0.8
+
+        if self.jump_speed < -self.JUMP_SPEED:
+            self.rect.y = self.Y_POS
+            self.dino_jump = False
+            self.jump_speed = self.JUMP_SPEED
 
     def draw(self, screen):
-        screen.blit(self.image, (self.dino_rect.x, self.dino_rect.y))
+        screen.blit(self.image, (self.rect.x, self.rect.y))
--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -31,7 +31,10 @@ class Game:
                 self.playing = False
 
     def update(self):
-        self.player.update()
+        user_input = pygame.key.get_pressed()
+        self.player.update(user_input)
+        
+
 
     def draw(self):
         self.clock.tick(FPS)
